04bb61dcdc7e5847118868f41b614efaa25270c0
Yuri Oliveira <yuri_souzaln@outlook.com>
2025-04-30 00:48:54 -0300

    MVP REPORT

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,4 +1,4 @@
-const { Events, MessageFlags, EmbedBuilder, ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
+const { Events, MessageFlags, EmbedBuilder, ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, time } = require('discord.js');
 const { execute } = require('./ready');
 
 module.exports = {
@@ -50,7 +50,47 @@ module.exports = {
                             console.log(err);
                         });
                 } else if (choice === 'report') {
-                    await interaction.reply('Escolhido report');
+
+                    const modalreport = new ModalBuilder()
+                        .setCustomId('modalreport')
+                        .setTitle('Report');
+
+                    const reportinput = new TextInputBuilder()
+                        .setCustomId('reportinput')
+                        .setLabel('Descreva em poucas palavras o ocorrido!')
+                        .setStyle(TextInputStyle.Paragraph)
+                        .setRequired(true);
+                    
+                    const actionReport = new ActionRowBuilder()
+                        .addComponents(reportinput);
+                    
+                    modalreport.addComponents(actionReport);
+
+                    const user = interaction.user;
+
+                    await interaction.showModal(modalreport);
+
+                    const modalfilter = ( interaction ) => interaction.customId === 'modalreport';
+
+                    interaction.awaitModalSubmit({ modalfilter, time: 360_000, })
+                    .then((result) => {
+                        const response = result.fields.getTextInputValue('reportinput');
+
+                        const channel = result.client.channels.cache.get('1366983996042510377');
+
+                        const embed = new EmbedBuilder()
+                            .setColor(0xFF0000)
+                            .setTitle('Report')
+                            .setAuthor({ name: user.username, iconURL: user.avatarURL() })
+                            .setDescription(response)
+                            .setTimestamp();
+
+                        channel.send({ embeds: [embed] });
+
+                        result.reply({ content: "Report enviada com sucesso!", ephemeral: true });
+                    }).catch((err) => {
+                        console.log(err);
+                    });
                 }
             }
         }
